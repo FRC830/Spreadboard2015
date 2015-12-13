7b8fea8d541b3bd8f4efee6c25ba34b166ea119c
830-R2 <R2@830.local>
2015-12-13 14:23:09 -0500

    add vectors

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -6,7 +6,10 @@ private:
 	static const int FIRST_VICTOR_PWM = 1;
 	static const int SECOND_VICTOR_PWM = 2;
 	static const int THRID_VICTOR_PWM = 3;
+
 	Victor*victorOne;
+	Victor*victorTwo;
+	Victor*victorThree;
 	LiveWindow *lw;
 
 
@@ -14,6 +17,8 @@ private:
 	{
 
 		victorOne = new Victor(FIRST_VICTOR_PWM);
+		victorTwo = new Victor(SECOND_VICTOR_PWM);
+		victorThree = new Victor(THRID_VICTOR_PWM);
 
 		lw = LiveWindow::GetInstance();
 	}
