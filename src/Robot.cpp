a708c89c6cc8be023fe3ca638d021a1de096e86e
cbott <cbott6@gmail.com>
2015-12-13 16:02:28 -0500

    basic pilot stuff

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,4 +1,6 @@
 #include "WPILib.h"
+#include "../util/830utilities.h"
+#include <map>
 
 class Robot: public IterativeRobot
 {
@@ -7,9 +9,10 @@ private:
 	static const int SECOND_VICTOR_PWM = 2;
 	static const int THRID_VICTOR_PWM = 3;
 
-	Victor*victorOne;
-	Victor*victorTwo;
-	Victor*victorThree;
+	Victor * victorOne;
+	Victor * victorTwo;
+	Victor * victorThree;
+	GamepadF310 * pilot;
 
 
 	void RobotInit()
@@ -18,6 +21,7 @@ private:
 		victorOne = new Victor(FIRST_VICTOR_PWM);
 		victorTwo = new Victor(SECOND_VICTOR_PWM);
 		victorThree = new Victor(THRID_VICTOR_PWM);
+		pilot = new GamepadF310(0);
 	}
 
 	void AutonomousInit()
@@ -37,7 +41,10 @@ private:
 
 	void TeleopPeriodic()
 	{
-
+		victorOne->Set(pilot->LeftY());
+		SmartDashboard::PutNumber("left y", pilot->LeftY());
+		SmartDashboard::PutNumber("victor channel", victorOne->GetChannel());
+		SmartDashboard::PutNumber("victor value", victorOne->Get());
 	}
 
 	void TestPeriodic()
